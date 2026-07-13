78891fefe8af2a852d82c98011f1804f86afce06
Joseph Norman <jnormantransactions@gmail.com>
2026-07-13 18:00:37 -0400

    parallel lines

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use bevy::{
+    asset::RenderAssetUsages,
     color::palettes::css::{GREEN, SEA_GREEN},
+    math::VectorSpace,
+    mesh::{Indices, PrimitiveTopology},
     prelude::*,
 };
 
@@ -14,23 +17,22 @@ fn main() {
 #[derive(Component)]
 pub struct Track;
 
-pub struct TrackGenerator {
+pub struct TrackBuilder {
     a: Vec2,
     b: Vec2,
 }
 
-impl TrackGenerator {
-    fn ends(a: Vec2, b: Vec2) -> Self {
+impl TrackBuilder {
+    fn new(a: Vec2, b: Vec2) -> Self {
         Self { a, b }
     }
 
-    fn generate(
+    fn bundle(
         self,
         meshes: &mut ResMut<Assets<Mesh>>,
         materials: &mut ResMut<Assets<ColorMaterial>>,
     ) -> impl Bundle {
-        // take both ends and move them so a is 0, 0.
-        let track = meshes.add(Segment2d::new(Vec2::new(0.0, 0.0), self.b - self.a));
+        let track = meshes.add(self.build());
 
         (
             Track,
@@ -38,6 +40,22 @@ impl TrackGenerator {
             Mesh2d(track),
             MeshMaterial2d(materials.add(Color::linear_rgba(255.0, 255.0, 255.0, 1.0))),
         )
+    }
+}
+
+const TRACK_WIDTH: f32 = 5.0;
+impl MeshBuilder for TrackBuilder {
+    fn build(&self) -> Mesh {
+        // normalize a and b to a
+        let a = Vec3::ZERO;
+        let b = (self.b - self.a).extend(0.0);
+        let offset = Vec2::new(0.0, TRACK_WIDTH).extend(0.0);
+        let positions = vec![a, b, a + offset, b + offset];
+        let indices = Indices::U32(vec![0, 1, 2, 3]);
+
+        Mesh::new(PrimitiveTopology::LineList, RenderAssetUsages::default())
+            .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, positions)
+            .with_inserted_indices(indices)
     }
 }
 
@@ -49,12 +67,12 @@ fn setup(
     commands.spawn((Camera2d, Camera::default()));
 
     let tracks = [
-        TrackGenerator::ends(Vec2::new(0.0, 0.0), Vec2::new(30.0, 30.0)),
-        TrackGenerator::ends(Vec2::new(-10.0, 10.0), Vec2::new(10.0, -10.0)),
-        TrackGenerator::ends(Vec2::new(-30.0, 0.0), Vec2::new(30.0, 30.0)),
+        TrackBuilder::new(Vec2::new(0.0, 0.0), Vec2::new(30.0, 30.0)),
+        TrackBuilder::new(Vec2::new(-10.0, 10.0), Vec2::new(10.0, -10.0)),
+        TrackBuilder::new(Vec2::new(-30.0, 0.0), Vec2::new(30.0, 30.0)),
     ];
     for track in tracks {
-        commands.spawn(track.generate(&mut meshes, &mut materials));
+        commands.spawn(track.bundle(&mut meshes, &mut materials));
     }
 
     let center = meshes.add(Circle::new(3.0));
