1dc7bc629d735bfae07a4502fa32fe0c5235ebe4
Joseph Norman <jnormantransactions@gmail.com>
2026-07-15 01:33:59 -0400

    separates track and mesh entities

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use bevy::{
     asset::RenderAssetUsages,
-    color::palettes::css::{GREEN, SEA_GREEN},
+    color::palettes::css::{GREEN, RED, SEA_GREEN},
     math::VectorSpace,
     mesh::{Indices, PrimitiveTopology},
     prelude::*,
@@ -9,49 +9,46 @@ use bevy::{
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
+        .add_systems(Startup, (setup, gen_track_mesh).chain())
         .add_systems(Update, rotate)
         .run();
 }
 
 #[derive(Component)]
-pub struct Track;
-
-pub struct TrackBuilder {
+pub struct Track {
     a: Vec2,
     b: Vec2,
 }
 
-impl TrackBuilder {
+impl Track {
     fn new(a: Vec2, b: Vec2) -> Self {
         Self { a, b }
     }
+}
 
-    fn bundle(
-        self,
-        meshes: &mut ResMut<Assets<Mesh>>,
-        materials: &mut ResMut<Assets<ColorMaterial>>,
-    ) -> impl Bundle {
-        let track = meshes.add(self.build());
+#[derive(Default)]
+pub struct TrackBuilder {
+    positions: Vec<Vec2>,
+    indices: Vec<u32>,
+}
 
-        (
-            Track,
-            Transform::from_xyz(self.a.x, self.a.y, 0.0),
-            Mesh2d(track),
-            MeshMaterial2d(materials.add(Color::linear_rgba(255.0, 255.0, 255.0, 1.0))),
-        )
+impl TrackBuilder {
+    fn add_track(&mut self, track: &Track) {
+        self.positions.push(track.a);
+        let a = self.positions.len();
+        self.positions.push(track.b);
+        let b = self.positions.len();
+
+        self.indices.push(a as u32);
+        self.indices.push(b as u32);
     }
 }
 
 const TRACK_WIDTH: f32 = 5.0;
 impl MeshBuilder for TrackBuilder {
     fn build(&self) -> Mesh {
-        // normalize a and b to a
-        let a = Vec3::ZERO;
-        let b = (self.b - self.a).extend(0.0);
-        let offset = Vec2::new(0.0, TRACK_WIDTH).extend(0.0);
-        let positions = vec![a, b, a + offset, b + offset];
-        let indices = Indices::U32(vec![0, 1, 2, 3]);
+        let positions: Vec<Vec3> = self.positions.iter().map(|p| p.extend(0.0)).collect();
+        let indices = Indices::U32(self.indices.clone());
 
         Mesh::new(PrimitiveTopology::LineList, RenderAssetUsages::default())
             .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, positions)
@@ -59,31 +56,44 @@ impl MeshBuilder for TrackBuilder {
     }
 }
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
+fn setup(mut commands: Commands) {
     commands.spawn((Camera2d, Camera::default()));
 
     let tracks = [
-        TrackBuilder::new(Vec2::new(0.0, 0.0), Vec2::new(30.0, 30.0)),
-        TrackBuilder::new(Vec2::new(-10.0, 10.0), Vec2::new(10.0, -10.0)),
-        TrackBuilder::new(Vec2::new(-30.0, 0.0), Vec2::new(30.0, 30.0)),
+        Track::new(Vec2::new(0.0, 10.0), Vec2::new(-10.0, -10.0)),
+        Track::new(Vec2::new(30.0, 0.0), Vec2::new(-30.0, 30.0)),
     ];
-    for track in tracks {
-        commands.spawn(track.bundle(&mut meshes, &mut materials));
-    }
 
+    for track in tracks {
+        commands.spawn(track);
+    }
+}
+
+fn gen_track_mesh(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    tracks: Query<&Track>,
+) {
     let center = meshes.add(Circle::new(3.0));
     commands.spawn((
         Mesh2d(center),
         MeshMaterial2d(materials.add(Color::Srgba(GREEN))),
     ));
 
-    let test_line = Segment2d::new(Vec2::new(-30.0, 0.0), Vec2::new(30.0, 30.0));
-    let mesh: Mesh = test_line.into();
-    println!("{:?}", mesh);
+    let mut track_builder = TrackBuilder::default();
+    for track in tracks {
+        track_builder.add_track(track);
+    }
+
+    let track_mesh = track_builder.build();
+    let track_id = meshes.add(track_mesh);
+
+    commands.spawn((
+        Mesh2d(track_id),
+        Transform::from_translation(Vec3::ZERO),
+        MeshMaterial2d(materials.add(Color::Srgba(RED))),
+    ));
 }
 
 fn rotate(mut tracks: Query<&mut Transform, With<Track>>) {
