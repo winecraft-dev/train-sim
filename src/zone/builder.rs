6499bb740ccedf1346d9af0f020bb4009e72312f
Joseph Norman <jnormantransactions@gmail.com>
2026-09-21 02:47:55 -0400

    trying to cram the signal construction into zone building

--- src/zone/builder.rs
+++ src/zone/builder.rs
@@ -2,8 +2,8 @@ use bevy::{ecs::system::SystemParam, prelude::*};
 
 use crate::{
     landmark::Landmark,
-    loc::FacingLocation,
-    zone::{AxleCounter, Zone, block::Block},
+    loc::{FacingLocation, cursor::TrackCursor},
+    zone::{AxleCounter, Zone, block::Block, junction::Junction},
 };
 
 #[derive(Resource, Default, Debug)]
@@ -15,18 +15,62 @@ pub struct ZoneStore {
 pub struct ZoneBuilder<'w, 's> {
     store: ResMut<'w, ZoneStore>,
     commands: Commands<'w, 's>,
+    cursor: TrackCursor<'w, 's>,
+}
+
+#[derive(Default)]
+pub struct ZoneConstructor {
+    entry_locs: Vec<FacingLocation>,
+    exit_locs: Vec<FacingLocation>,
+}
+
+impl ZoneConstructor {
+    pub fn new() -> Self {
+        Self { ..default() }
+    }
+
+    pub fn add_entry(mut self, floc: FacingLocation) -> Self {
+        self.entry_locs.push(floc);
+        self
+    }
+
+    pub fn add_exit(mut self, floc: FacingLocation) -> Self {
+        self.exit_locs.push(floc);
+        self
+    }
 }
 
 #[derive(Event)]
 pub struct ZonesBuilt;
 
 impl<'w, 's> ZoneBuilder<'w, 's> {
-    pub fn new(&mut self) -> Entity {
+    pub fn new(&mut self, zone: ZoneConstructor) -> Entity {
         let e_zone = self
             .commands
             .spawn((GlobalTransform::default(), Zone::default()))
             .id();
-        self.store.zones.push(e_zone);
+
+        let mut e_entrys: Vec<Entity> = Vec::new(); // misspelled for alignment
+        let mut e_exits: Vec<Entity> = Vec::new();
+
+        // check for if junction or block given how many entrys/exits
+        for entry_loc in zone.entry_locs.iter() {
+            let e_entry = self
+                .commands
+                .spawn((AxleCounter { zone: e_zone }, Landmark, *entry_loc))
+                .id();
+            e_entrys.push(e_entry);
+        }
+
+        for exit_loc in zone.exit_locs.iter() {
+            let e_exit = self
+                .commands
+                .spawn((AxleCounter { zone: e_zone }, Landmark, *exit_loc))
+                .id();
+            e_exits.push(e_exit);
+        }
+
+        let signal_loc = self.cursor.traverse(zone.entry_locs[0], -15.0).unwrap();
 
         e_zone
     }
@@ -36,13 +80,5 @@ impl<'w, 's> ZoneBuilder<'w, 's> {
         let e_counter = self.commands.spawn((counter, Landmark, floc)).id();
 
         self.commands.entity(zone).add_child(e_counter);
-    }
-
-    pub fn block(&mut self, zone: Entity) {
-        self.commands.entity(zone).insert(Block::default());
-    }
-
-    pub fn flush(&mut self) {
-        self.commands.trigger(ZonesBuilt);
     }
 }
